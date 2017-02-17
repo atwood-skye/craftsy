581a89320846b2015e216ae533aa762784ef9e66
Skye Atwood <atwood.skye@gmail.com>
2017-02-17 13:21:14 -0800

    pass spec for displaying products with home controller

--- spec/features/home_controller_integration_spec.rb
+++ spec/features/home_controller_integration_spec.rb
@@ -5,5 +5,19 @@ describe HomeController do
     visit '/'
     expect(page).to have_content 'Our Products'
   end
+  it "will show all products" do
+    user = FactoryGirl.create(:user)
+    visit '/'
+    click_on 'Sign In'
+    fill_in "Email", :with => user.email
+    fill_in "Password", :with => user.password
+    click_on 'Log in'
+    click_on 'Add Product'
+    fill_in "Name", :with => "Painting"
+    fill_in "Description", :with => "Beautiful"
+    fill_in "Price", :with => 25
+    click_on 'Submit'
+    expect(page).to have_content 'Beautiful'
+  end
 
 end
